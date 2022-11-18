d6df1f38e10382f774cc1e39707ba4450b8bbdc9
Niall Colman <niallcolman@Nialls-MacBook-Pro.local>
2022-11-18 19:20:55 +0000

    html markup and tailwindcss setup introduced

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        title: ["Formular Medium"],
+        body: ["Formular"],
+      },
+      colors: {
+        armleyGreen: "#566548",
+        armleyBlack: "#0f0d0c",
+        armleyNeon: "#e5e771",
+        armleyWhite: "#ffffff",
+      },
+      lineHeight: {
+        1.2: "1.2",
+      },
+      screens: {
+        specialScreen: "1500px",
+      },
+    },
+  },
+  plugins: [],
+};
